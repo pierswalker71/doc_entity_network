073c903952067beacf9bedb9e0cca466ed7ed7c5
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-12-27 21:41:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ def main():
     # Imports
     import pandas as pd
     
+    import spacy
     nlp = spacy.load("en_core_web_md")
 
     #==============================================================================
