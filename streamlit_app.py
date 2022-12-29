65bd2c0fd25e4a745cba609686c72404a4c1f036
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-12-29 09:47:28 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,8 +177,8 @@ def main():
 
     color_map = ['red' if node in top_nodes else 'b' for node in G]
     pos = nx.spring_layout(G, k=0.15, iterations=20)
-    #pos=pos,, node_size=50
-    nx.draw(G, ax=ax,node_color=color_map, with_labels=True, font_weight='bold',font_size=6)
+    #pos=pos,, node_size=50, with_labels=True, font_weight='bold',font_size=6
+    nx.draw(G, ax=ax,node_color=color_map)
     st.pyplot(fig)
     
     
