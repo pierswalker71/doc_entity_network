ceeeb1bb864a8cac120689d91f59dda942a22fd4
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-12-29 13:42:18 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,10 +87,10 @@ def main():
         
     # Display example NER
     st.write('Named entity recognition example')
-    eg_text_row = st.slider('Example row ID',0,data.shape[0]-1,0)
+    eg_text_row = st.slider('Example row ID',0,len(data.index)-1,0)
     #with st.expander('Named entity recognition example'):
-    eg_text_length = st.slider('Example text length',5,len(data.iloc[eg_text_row,0]),min(200,len(data.iloc[eg_text_row,0]))
-    st.write(eg_text_length)
+    eg_text_length = st.slider('Example text length',5,len(data.iloc[eg_text_row,0]),10)
+    st.write(str(eg_text_length))
     doc_example = nlp(data.iloc[eg_text_row,0][:200])
     visualize_ner(doc_example, labels=nlp.get_pipe("ner").labels,title='', show_table=False)
     #https://github.com/explosion/spacy-streamlit
