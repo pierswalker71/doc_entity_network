c57906346bce86ebb0cabc38b0229e503dffb04a
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2022-12-31 16:29:18 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -231,8 +231,8 @@ def main():
 
     fig, ax = plt.subplots(figsize=(12, 12), dpi=300)
     color_map = ['red' if node in top_nodes else 'b' for node in H]
-    nx.draw(h, pos=pos, ax=ax, edge_color='black' ,width=1, linewidths=1, node_size=10,
-            node_color=color_map, with_labels=True, font_weight='normal', font_size=12)
+    nx.draw(H, pos=pos, ax=ax, edge_color='black' ,width=1, linewidths=1, node_size=10,
+            #node_color=color_map, with_labels=True, font_weight='normal', font_size=12)
     st.pyplot(fig)
 
 
