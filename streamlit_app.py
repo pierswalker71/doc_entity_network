a57a944fc0d37d83ceaadfd0f6fe20220c31139f
pierswalker71 <60096809+pierswalker71@users.noreply.github.com>
2023-01-01 13:35:01 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -128,7 +128,7 @@ def main():
 
     #for row_id in range(data.shape[0]-1):
     #for row_id in range(len(data.index)-1):
-    for row_id in range(50)
+    for row_id in range(50):
         doc = nlp(data.iloc[:,0][row_id])
         page_title = str(row_id)
         #doc.user_data['title'] = page_title
